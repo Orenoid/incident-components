92d6bcc11abdb0ccd986e2d6ec594c35204b3a53
Orenoid <stefu1225@gmail.com>
2025-05-10 09:01:46 +0800

    test components

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,5 @@
-// import { HelloDiv } from "./components"
 export { HelloDiv } from "./components"
-
+export { TestComponent } from "./test/components"
 export function HelloWorld() {
     return "Hello World v2"
 }
